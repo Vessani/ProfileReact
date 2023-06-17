8db3e54c401f18b298c03193fc1a23a37c07dd69
Alan <alanmichel93@gmail.com>
2023-06-17 19:38:19 -0300

    Incluindo Children

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,18 +1,25 @@
+import LinkButton from "./LinkButton"
+import ProfileSection from "./ProfileSection"
+import Title from "./Title"
 import styles from "./styles.module.css"
 
 export default function Profile(props) {
-    return (
-        <div className={styles.container}>
-            <img className={styles.avatar} src={props.avatar} alt={props.name} />
-            <h2 className={styles.name}>{props.name}</h2>
-            <div>{props.bios}</div>
-            <div>{props.phone}</div>
-            <div>{props.email}</div>
-            <div className={styles.links}>
-                <a href={props.githubUrl} target="_blank">GitHub</a>
-                <a href={props.linkedinUrl} target="_blank">LindedIn</a>
-                <a href={props.twitterUrl} target="_blank">Twitter</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <img className={styles.avatar} src={props.avatar} alt={props.name} />
+      <Title>
+        {props.name}        
+      </Title>
+      <ProfileSection>{props.bios}</ProfileSection>
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection>
+        <div className={styles.links}>
+            <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
+            <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
+            <LinkButton href={props.twitterUrl} target="_blank">Twitter</LinkButton>
+        </div>    
+      </ProfileSection>
+    </div>
+  )
 }
